f835fd8e7fd2ac36895dddb2d2a77ddeed67d1b5
Fulin Li <fulin.li@thoughtworks.com>
2020-05-23 20:30:05 +0800

    [Fulin Xiaofeng] should bag stored in locker A and return a ticket when store bag given smart robot, locker A whose space left is 3 and locker B whose space left is 2

--- __tests__/locker_robot/smartLockerRobot.test.js
+++ __tests__/locker_robot/smartLockerRobot.test.js
@@ -11,3 +11,14 @@ test('should bag stored in locker B and return a ticket when store bag given sma
 
   expect(lockerB.retrieve(ticket)).toBe(bag);
 });
+
+test('should bag stored in locker A and return a ticket when store bag given smart robot, locker A whose space left is 3 and locker B whose space left is 2', () => {
+  const lockerA = new Locker(3);
+  const lockerB = new Locker(2);
+  const smartLockerRobot = new SmartLockerRobot(lockerA, lockerB);
+  const bag = new Bag();
+
+  const ticket = smartLockerRobot.store(bag);
+
+  expect(lockerA.retrieve(ticket)).toBe(bag);
+});
