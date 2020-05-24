4c2fc7e925462caf5134f545972b37634bff3b08
Fulin Li <fulin.li@thoughtworks.com>
2020-05-24 16:33:39 +0800

    [Fulin Xiaofeng] should bag stored in locker A or locker B when space is same

--- __tests__/locker_robot/smartLockerRobot.test.js
+++ __tests__/locker_robot/smartLockerRobot.test.js
@@ -23,6 +23,17 @@ test('should bag stored in locker A and return a ticket when store bag given sma
   expect(lockerA.retrieve(ticket)).toBe(bag);
 });
 
+test('should bag stored in locker A or locker B and return a ticket when store bag given smart robot, locker A whose space left is 2 and locker B whose space left is 2', () => {
+  const lockerA = new Locker(2);
+  const lockerB = new Locker(2);
+  const smartLockerRobot = new SmartLockerRobot(lockerA, lockerB);
+  const bag = new Bag();
+
+  const ticket = smartLockerRobot.store(bag);
+
+  expect([lockerA.retrieve(ticket), lockerB.retrieve(ticket)]).toContain(bag);
+});
+
 test('should store failed and send a message "No space left!" when store bag given smart robot, locker A whose space left is 0 and locker B whose space left is 0', () => {
   const lockerA = new Locker(1);
   const lockerB = new Locker(1);
--- task/task-smart-locker-robot.md
+++ task/task-smart-locker-robot.md
@@ -1,5 +1,6 @@
 1.Given smart robot, locker A whose space left is 2 and locker B whose space left is 3, When store bag then bag stored in locker B and return a ticket.
 2.Given smart robot, locker A whose space left is 3 and locker B whose space left is 2, When store bag then bag stored in locker A and return a ticket.
+4.Given smart robot, locker A whose space left is 2 and locker B whose space left is 2, When store bag then bag stored in locker A or locker B and return a ticket.
 3.Given smart robot and locker A,B both have no space, When store bag, Then store failed and send a message "No space left!".
 4.Given smart robot and valid ticket, When retrieve bag, Then retrieve successfully and send the bag.
 5.Given smart robot and invalid ticket, When retrieve bag, Then retrieve failed and send a message "Please input valid ticket!".
