74572b19943f002a854ed084a34b075d8517bf2a
Xiaofeng Su <xiaofeng.su@thoughtworks.com>
2020-05-29 20:37:25 +0800

    [Fulin Xiaofeng]: should stored in locker A and return a ticket when store given locker A space left 3 size 4 and locker B space left 2 size 10

--- __tests__/locker_robot/superLockerRobot.test.js
+++ __tests__/locker_robot/superLockerRobot.test.js
@@ -56,3 +56,16 @@ test('should stored in locker B and return a ticket when store given locker A sp
 
   expect(lockerB.retrieve(ticket)).toBe(bag);
 });
+
+test('should stored in locker A and return a ticket when store given locker A space left 3 size 4 and locker B space left 2 size 10', () => {
+  const lockerA = new Locker(4);
+  const lockerB = new Locker(10);
+  lockerA.store(new Bag());
+  batchStoreBag(lockerB, 8);
+  const superLockerRobot = new SuperLockerRobot(lockerA, lockerB);
+  const bag = new Bag();
+
+  const ticket = superLockerRobot.store(bag);
+
+  expect(lockerA.retrieve(ticket)).toBe(bag);
+});
